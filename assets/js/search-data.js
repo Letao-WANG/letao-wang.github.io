cdc485c0073e61c960f9698246625daa47bfbd88
Letao-WANG <55490010+Letao-WANG@users.noreply.github.com>
2026-04-11 18:41:13 +0000

    Deploying to gh-pages from @ Letao-WANG/letao-wang.github.io@770b6ada1d828a5d4648755165c1642a1bd85d56 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,16 +42,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-best-poster-award-of-the-quantum-saclay-day-for-my-work-on-learning-pdes-for-portfolio-optimization-with-quantum-physics-informed-neural-networks",
+          title: 'Best Poster Award of the Quantum-Saclay Day for my work on Learning PDEs...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
